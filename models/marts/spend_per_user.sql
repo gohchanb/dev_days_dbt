c0b37bd5e5fed85b6015492b84b6dcf28cb9d6f1
Benedict Chan <benedict@etleap.com>
2025-02-11 11:55:57 +0000

    make build succeed

--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -2,5 +2,5 @@ SELECT p.user_id, SUM(i.price) AS spend
   FROM {{source('benedict_test', 'PURCHASE')}} p
     INNER JOIN {{source('benedict_test', 'LINE_ITEM')}} li ON li.purchase_id = p.id
     INNER JOIN {{source('benedict_test', 'ITEM')}} i ON i.item_id = li.item_id
-    INNER JOIN benedict_test.USER u ON u.id = p.user_id
+    -- INNER JOIN benedict_test.USER u ON u.id = p.user_id
   GROUP BY p.user_id
